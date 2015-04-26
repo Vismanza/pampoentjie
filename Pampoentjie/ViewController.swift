2ac1bf83467c189829ce6f896ef44d4c7f5cb5e7
Andrew Visser <andrew.visser@opower.com>
2015-04-26 14:01:33 +0100

    Initial Commit

---
+++ Pampoentjie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pampoentjie
+//
+//  Created by Andrew Visser on 26/04/2015.
+//  Copyright (c) 2015 Andrew Visser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

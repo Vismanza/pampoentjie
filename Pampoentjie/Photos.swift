ed2250731d5937c783db094c31d70d6785d723ae
Andrew Visser <andrew.visser@opower.com>
2015-04-26 14:48:36 +0100

    Defined main storyboard and created controller files.

---
+++ Pampoentjie/Photos.swift
@@ -0,0 +1,15 @@
+//
+//  Photos.swift
+//  Pampoentjie
+//
+//  Created by Andrew Visser on 26/04/2015.
+//  Copyright (c) 2015 Andrew Visser. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+    var name : String
+    var filename : String
+    var note : String
+}
